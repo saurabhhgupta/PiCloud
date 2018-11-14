614ba0d1fc753c05438906e871c8736d05bfbf97
Saurabh Gupta <guptahsaurabh@gmail.com>
2018-11-14 10:27:20 -0800

    Add threading capability for periodic reading

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 
 import os
 import time
+import threading
+import paho.mqtt.client as mqtt
+
+INTERVAL = 3.0
 
 def measure_cpu():
 	temp = os.popen("cat /sys/class/thermal/thermal_zone0/temp").readline()
@@ -11,5 +15,14 @@ def measure_gpu():
 	temp = os.popen("vcgencmd measure_temp").readline()
 	return float((temp.replace("temp=", "").replace("'C", "")))
 
-print("CPU temperature", measure_cpu())
-print("GPU temperature", measure_gpu())
+def display_temps():
+	global INTERVAL
+	threading.Timer(INTERVAL, display_temps).start()
+	print("CPU temperature:", measure_cpu())
+	print("GPU temperature:", measure_gpu())
+
+def main():
+	display_temps()
+
+if __name__ == '__main__':
+	main()
